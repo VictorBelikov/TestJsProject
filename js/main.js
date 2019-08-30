1101e9a70d37967b348bf43f4634effba58e6380
Victor Belikov <v1ctor.belikov@gmail.com>
2019-08-30 11:02:38 +0300

    feat: add SyFy grid

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
-  $('.test').click(function() {
-    $('body').prepend('<button class="hello">click me!</button>');
-    $('button.hello').click(function() {
-      console.log('Hello, world!');
-    });
-  });
+  $('#next').click(function () {
+    $pdk.controller.next(true);
+  })
+
+  $('#prev').click(function () {
+    $pdk.controller.previous(true);
+  })
 });
-
-// var cpcplayer = USAN.cpcplayers[playerId];
-// https://sp.auth.adobe.com/adobe-services/authenticate/saml?noflash=true&mso_id=Cablevision&requestor_id=usa&no_iframe=false&domain_name=adobe.com
-
-// ======================================================
